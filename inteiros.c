153102e8a04c5b56db05e4b154301a3353ed32a4
MoyAlmas <portugal210@gmail.com>
2018-03-15 15:58:10 -0100

    primeiro ex inteiros

--- inteiros.c
+++ inteiros.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+#include <inteiros.h>
 
 void imprime(int v[]){
 	for(int i=0; i<10; i++){
@@ -7,28 +8,28 @@ void imprime(int v[]){
 	}
 }
 
-void imprime2(float v[]){
-	for(int i=0; i<10; i++){
-		printf("%.2->",v[i]);	
-	}
-}
+//void imprime2(float v[]){
+//	for(int i=0; i<10; i++){
+//		printf("%.2->",v[i]);	
+//	}
+//}
 
 void main(){
 	int v;
 	int i = 0;
-	float b = 0;
+//	float b = 0;
 	char vector[10];
 	int v_i[10];
-	float v_j[10];
+//	float v_j[10];
 	printf("Insira os valores inteiros");
 	for(int i=1; i<11; i++){
 		fgets(vector, 10, stdin);
 		v = atoi(vector);
 		b = (float) v;
 		v_i[10-i]=v;
-		v_j[10-i]=v;
+//		v_j[10-i]=v;
 	}
 	imprime(v_i);
-	imprime2(v_j);
+//	imprime2(v_j);
 }
 	
